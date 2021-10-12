23ff22591d885b469e3037b09dde57968b6d938d
oskar zdziarski <oskarzdz@gmail.com>
2021-10-12 10:58:45 -0700

    move run script

--- Blackjack/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ main.cpp game.cpp player.cpp Deck/card.cpp Deck/deck.cpp -std=c++11 -o Blackjack
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ Blackjack/main.cpp Blackjack/game.cpp Blackjack/player.cpp Blackjack/Deck/card.cpp Blackjack/Deck/deck.cpp -std=c++11 -o GAME
